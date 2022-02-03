e3e0f60e9c7439577743fa28b739ea198d7b2df6
GT Lakshan <94892809+gtlakshan@users.noreply.github.com>
2022-02-03 22:38:00 +0530

    New

---
+++ android/app/src/main/kotlin/com/infinitystock/Kuniverse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.infinitystock.kuniverse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/config.dart
@@ -0,0 +1,21 @@
+class Config {
+  final String hashTag = '#k-universe';
+  final String appName = 'K-Universe';
+
+  final String packageName = 'com.infinitystock.kuniverse';
+  final String appVersion = '1.0';
+
+  final String appIcon = 'assets/images/icon.png';
+  final String splashIcon = 'assets/images/splash.png';
+  final String guestAvatar = 'assets/images/guest_avatar.png';
+
+  // -- Onesignal Push Notification
+  final String onesignalAppId = '0a72f529-b3c1-4162-a07e-ad4fd0900ee9';
+
+  //-- admob ads --
+  final String admobAppId = 'ca-app-pub-3940256099942544~3347511713';
+  final String admobInterstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+
+  //-- facebook ads--
+  final String facebookInterstitialAdId = '5878408****************';
+}
