787bcc39d5ebff4b6d01f5dfc1406809e63abf07
GT Lakshan <94892809+gtlakshan@users.noreply.github.com>
2022-04-24 18:19:39 +0530

    Ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:js';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:wallpaper_app/blocs/ads_bloc.dart';
+import 'package:wallpaper_app/models/config.dart';
 import './blocs/bookmark_bloc.dart';
 import './blocs/data_bloc.dart';
 import './blocs/internet_bloc.dart';
@@ -12,16 +16,12 @@ import './blocs/userdata_bloc.dart';
 import './pages/home.dart';
 import './pages/sign_in_page.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await MobileAds.instance.initialize();
   await Firebase.initializeApp();
-  await FlutterDownloader.initialize(
-    debug: true
-  );
+  await FlutterDownloader.initialize(debug: true);
   runApp(MyApp());
-  
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -43,15 +43,12 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider<BookmarkBloc>(
             create: (context) => BookmarkBloc(),
           ),
-          
           ChangeNotifierProvider<InternetBloc>(
             create: (context) => InternetBloc(),
           ),
-
           ChangeNotifierProvider<AdsBloc>(
             create: (context) => AdsBloc(),
           ),
-
         ],
         child: MaterialApp(
             debugShowCheckedModeBanner: false,
@@ -61,14 +58,13 @@ class MyApp extends StatelessWidget {
                 systemOverlayStyle: SystemUiOverlayStyle(
                   statusBarIconBrightness: Brightness.dark,
                   statusBarColor: Colors.transparent,
-
                 ),
                 color: Colors.white,
                 titleTextStyle: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w600),
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600),
                 elevation: 0,
                 iconTheme: IconThemeData(
                   color: Colors.black,
@@ -86,9 +82,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
 class MyApp1 extends StatelessWidget {
   const MyApp1({Key? key}) : super(key: key);
 
@@ -96,6 +89,7 @@ class MyApp1 extends StatelessWidget {
   Widget build(BuildContext context) {
     final sb = context.watch<SignInBloc>();
     return sb.isSignedIn == false && sb.guestUser == false
-    ? SignInPage() : HomePage();
+        ? SignInPage()
+        : HomePage();
   }
 }
